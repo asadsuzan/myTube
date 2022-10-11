99b1552c6fc7b30f13c592f6b9d1112a787a57ef
suzan <asadsuzan7@gmail.com>
2022-10-11 14:16:59 +0600

    modified readMe

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import ChenelDetails from "./components/ChenelDetails";
 import Feeds from "./components/Feeds";
 import NavBar from "./components/NavBar";
+import NotFound from "./components/NotFound";
 import SearchFeed from "./components/SearchFeed";
 import VideoDetails from "./components/VideoDetails";
 
@@ -16,6 +17,7 @@ function App() {
       <Route path="chenel/:id" element={<ChenelDetails/>}/>
       <Route path="video/:id" element={<VideoDetails/>}/>
       <Route path="search/:searchFor" element={<SearchFeed/>}/>
+      <Route path="*" element={<NotFound/>}/>
      </Routes> 
  
    </>
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='not-found'>
+    <img src="https://www.pngitem.com/pimgs/m/255-2550411_404-error-images-free-png-transparent-png.png" alt="not-found" className='w-100' />
+      
+    </div>
+  )
+}
+
+export default NotFound
